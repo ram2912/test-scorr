8d0146cea1b6107ec37e026c05113a266730c003
ram2912 <shrirampawar@Shrirams-MacBook-Pro-2.local>
2023-04-26 20:18:00 +0200

    Ready for prod2

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import Link from 'next/link';
-require('dotenv').config();
+
 
 const inter = Inter({ subsets: ['latin'] })
 
