ce9f97632c164984cf482a902318d93fb8d33205
ram2912 <shrirampawar@Shrirams-MacBook-Pro-2.local>
2023-05-29 11:19:50 +0200

    test

--- src/pages/Products/funnelTable.js
+++ src/pages/Products/funnelTable.js
@@ -15,16 +15,18 @@ const PipelineForm = () => {
       const response = await fetch('https://backend.scorr-app.eu/pipelines2', {
         credentials: 'include',
       });
-      const json = await response.json(); // Parse the response body as JSON
-      const data = JSON.parse(json); // Parse the JSON data
+      const data = await response.json(); // Parse the response body as JSON
   
-      const pipelineNames = data.results.map((pipeline) => pipeline.label);
-      setPipelines(pipelineNames);
+      const pipelineData = data.results.map((pipeline) => ({
+        id: pipeline.id,
+        name: pipeline.label,
+      }));
+  
+      setPipelines(pipelineData);
     } catch (error) {
       console.error('Error fetching pipelines:', error);
     }
   };
-  
 
   const handleLeadPipelineChange = (event) => {
     setLeadPipeline(event.target.value);
@@ -38,16 +40,47 @@ const PipelineForm = () => {
     setSalesPipeline(event.target.value);
   };
 
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
-
-    // Do something with the selected pipelines (e.g., make an API request)
-    console.log('Selected Pipelines:', {
-      leadPipeline,
-      bdrPipeline,
-      salesPipeline,
-    });
-
+  
+    const pipelineIds = pipelines
+      .filter((pipeline) => [leadPipeline, bdrPipeline, salesPipeline].includes(pipeline.name))
+      .map((pipeline) => pipeline.id);
+  
+    const data = {
+      leadPipeline: {
+        id: pipelineIds[0],
+        name: leadPipeline,
+      },
+      bdrPipeline: {
+        id: pipelineIds[1],
+        name: bdrPipeline,
+      },
+      salesPipeline: {
+        id: pipelineIds[2],
+        name: salesPipeline,
+      },
+    };
+  
+    try {
+      const response = await fetch('https://backend.scorr-app.eu/store-pipelines', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        credentials: 'include',
+        body: JSON.stringify(data), // Convert the data to JSON format
+      });
+  
+      if (response.ok) {
+        console.log('Pipelines stored in the database successfully!');
+      } else {
+        console.error('Error storing pipelines:', response.status);
+      }
+    } catch (error) {
+      console.error('Error storing pipelines:', error);
+    }
+  
     // Reset the form
     setLeadPipeline('');
     setBdrPipeline('');
@@ -60,9 +93,9 @@ const PipelineForm = () => {
         <label htmlFor="lead-pipeline">Lead Pipeline:</label>
         <select id="lead-pipeline" value={leadPipeline} onChange={handleLeadPipelineChange}>
           <option value="">Select Lead Pipeline</option>
-          {pipelines.map((pipeline, index) => (
-            <option key={index} value={pipeline}>
-              {pipeline}
+          {pipelines.map((pipeline) => (
+            <option key={pipeline.id} value={pipeline.id}>
+            {pipeline.name}
             </option>
           ))}
         </select>
@@ -72,9 +105,9 @@ const PipelineForm = () => {
         <label htmlFor="bdr-pipeline">BDR Pipeline:</label>
         <select id="bdr-pipeline" value={bdrPipeline} onChange={handleBdrPipelineChange}>
           <option value="">Select BDR Pipeline</option>
-          {pipelines.map((pipeline, index) => (
-            <option key={index} value={pipeline}>
-              {pipeline}
+          {pipelines.map((pipeline) => (
+            <option key={pipeline.id} value={pipeline.id}>
+            {pipeline.name}
             </option>
           ))}
         </select>
@@ -84,9 +117,9 @@ const PipelineForm = () => {
         <label htmlFor="sales-pipeline">Sales Pipeline:</label>
         <select id="sales-pipeline" value={salesPipeline} onChange={handleSalesPipelineChange}>
           <option value="">Select Sales Pipeline</option>
-          {pipelines.map((pipeline, index) => (
-            <option key={index} value={pipeline}>
-              {pipeline}
+          {pipelines.map((pipeline) => (
+            <option key={pipeline.id} value={pipeline.id}>
+            {pipeline.name}
             </option>
           ))}
         </select>
