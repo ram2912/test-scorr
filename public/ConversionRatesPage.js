1e9b74fe1368b4b4ae8e3467d9948cab8650eb5a
ram2912 <shrirampawar@Shrirams-MacBook-Pro-2.local>
2023-06-06 21:42:17 +0200

    with AI suggestions

--- public/ConversionRatesPage.js
+++ public/ConversionRatesPage.js
@@ -21,57 +21,69 @@ export default function ConversionRatesPage({ conversionRates }) {
     };
 
     const suggestedColumns = [
-        { name: 'Status', data: ['At Risk', 'On Track'] },
-        { name: 'Reason', data: ['High Competition', 'Poor Marketing Strategy'] },
+        { name: 'Status', data: ['At Risk'] },
+        { name: 'Reason', data: ['High Competition'] },
         // Add more suggested columns here
       ];
   
     // Rest of the code...
   
     return (
-      <div>
-        <table className={styles.conversionRatesTable}>
-          <thead>
-            <tr>
-              <th>Source Stage</th>
-              <th>Target Stage</th>
-              <th>Conversion Rate</th>
-              {suggestedColumns.map((column, index) => (
-                <th
-                  key={index}
-                  onClick={() => handleColumnClick(column.name)}
-                  style={{
-                    cursor: 'pointer',
-                    opacity: isColumnSelected(column.name) ? 1 : 0.5,
-                    transition: 'opacity 0.3s',
-                  }}
-                >
-                  {column.name}
-                </th>
-              ))}
-            </tr>
-          </thead>
-          <tbody>
-            {conversionRates.map((rate, index) => (
-              <tr key={index}>
-                <td style={{ width: '150px', fontSize: 'small' }}>{rate.sourceStage.name}</td>
-                <td style={{ width: '150px', fontSize: 'small' }}>{rate.targetStage.name}</td>
-                <td style={{ width: '80px', fontSize: 'small' }}>{rate.conversionRate}</td>
+        <div>
+          <table className={styles.conversionRatesTable}>
+            <thead>
+              <tr>
+                <th>Source Stage</th>
+                <th>Target Stage</th>
+                <th>Conversion Rate</th>
                 {suggestedColumns.map((column, index) => (
-                  <td key={index} colSpan={3} style={{ opacity: isColumnSelected(column.name) ? 1 : 0 }}>
-                    {isColumnSelected(column.name) &&
-                      column.data.map((data, dataIndex) => (
-                        <div key={dataIndex}>{data}</div>
-                      ))}
-                  </td>
+                  <th
+                    key={index}
+                    onClick={() => handleColumnClick(column.name)}
+                    style={{
+                      cursor: 'pointer',
+                      opacity: isColumnSelected(column.name) ? 1 : 0.5,
+                      transition: 'opacity 0.3s',
+                      backgroundColor: isColumnSelected(column.name) ? '#6e6e6e' : 'transparent',
+                    }}
+                  >
+                    {column.name}
+                  </th>
                 ))}
               </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-    );
-  }
+            </thead>
+            <tbody>
+              {conversionRates.map((rate, index) => (
+                <tr key={index}>
+                  <td style={{ width: '150px', fontSize: 'small' }}>{rate.sourceStage.name}</td>
+                  <td style={{ width: '150px', fontSize: 'small' }}>{rate.targetStage.name}</td>
+                  <td style={{ width: '80px', fontSize: 'small' }}>{rate.conversionRate}</td>
+                  {suggestedColumns.map((column, index) => (
+                    <td key={index} colSpan={1} style={{ cursor: 'pointer', transition: 'opacity 0.3s', backgroundColor: isColumnSelected(column.name) ? '#454545' : '#010102', }}>
+                      {
+                        column.data.map((data, dataIndex) => (
+                            <div
+                            key={dataIndex}
+                            
+                            style={{
+                              opacity: isColumnSelected(column.name) ? 1 : 0.3,
+                              
+                              transition: 'opacity 0.3s',
+                            }}
+                          >
+                            {data}
+                          </div>
+                          
+                        ))}
+                    </td>
+                  ))}
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      );
+    }
   
 
 
