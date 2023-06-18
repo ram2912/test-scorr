701d50fd701561f3e9a15a30098e1d82750e7967
ram2912 <shrirampawar@Shrirams-MacBook-Pro-2.local>
2023-06-18 15:01:58 +0200

    test

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     env: {
      OPENAI_API: process.env.NEXT_PUBLIC_OPENAI_API_KEY,
-     NODE_ENV: 'development'
+     NODE_ENV: process.env.NEXT_PUBLIC_NODE_ENV,
     },
   };
   
