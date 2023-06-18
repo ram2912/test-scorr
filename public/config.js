664f61297ebac7875eae3ee9d8a788facb4a4976
ram2912 <shrirampawar@Shrirams-MacBook-Pro-2.local>
2023-06-18 15:24:11 +0200

    test

--- public/config.js
+++ public/config.js
@@ -1,24 +1,31 @@
 const devConfig = {
-    endpoints: {
-      funnelUrl: 'https://testback.scorr-app.eu/funnels', // Development API endpoint URL
-      pipelineStagesUrl: 'https://testback.scorr-app.eu/pipelines-stages',
-      conversionRateUrl:'https://testback.scorr-app.eu/conversion-rate',
-      pipelinesUrl:'https://testback.scorr-app.eu/pipelines2',
-      storePipelinesUrl:'https://testback.scorr-app.eu/store-pipelines',
-    },
-  };
-  
-  const prodConfig = {
-    endpoints: {
-      funnelUrl: 'https://backend.scorr-app.eu/funnels', // Development API endpoint URL
-      pipelineStagesUrl: 'https://backend.scorr-app.eu/pipelines-stages',
-      conversionRateUrl:'https://backend.scorr-app.eu/conversion-rate',
-      pipelinesUrl:'https://backend.scorr-app.eu/pipelines2',
-      storePipelinesUrl:'https://backend.scorr-app.eu/store-pipelines',// Production another endpoint URL
-    },
-  };
-  
-  const config = process.env.NODE_ENV === 'development' ? devConfig : prodConfig;
-  
-  export default config;
-  
+  endpoints: {
+    funnelUrl: 'https://testback.scorr-app.eu/funnels', // Development API endpoint URL
+    pipelineStagesUrl: 'https://testback.scorr-app.eu/pipelines-stages',
+    conversionRateUrl: 'https://testback.scorr-app.eu/conversion-rate',
+    pipelinesUrl: 'https://testback.scorr-app.eu/pipelines2',
+    storePipelinesUrl: 'https://testback.scorr-app.eu/store-pipelines',
+  },
+};
+
+const prodConfig = {
+  endpoints: {
+    funnelUrl: 'https://backend.scorr-app.eu/funnels', // Production API endpoint URL
+    pipelineStagesUrl: 'https://backend.scorr-app.eu/pipelines-stages',
+    conversionRateUrl: 'https://backend.scorr-app.eu/conversion-rate',
+    pipelinesUrl: 'https://backend.scorr-app.eu/pipelines2',
+    storePipelinesUrl: 'https://backend.scorr-app.eu/store-pipelines', // Production another endpoint URL
+  },
+};
+
+const config = {
+  endpoints: {},
+};
+
+if (process.env.NODE_ENV === 'development') {
+  config.endpoints = devConfig.endpoints;
+} else {
+  config.endpoints = prodConfig.endpoints;
+}
+
+export default config;
