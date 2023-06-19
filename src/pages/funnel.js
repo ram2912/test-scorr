61cc00722cdc9604b6b3f63539ca27fdb71d0f3f
ram2912 <shrirampawar@Shrirams-MacBook-Pro-2.local>
2023-06-19 17:45:29 +0200

    test

--- src/pages/funnel.js
+++ src/pages/funnel.js
@@ -193,13 +193,13 @@ console.log(showNextStep);
 <h1 style={{ fontSize: '18px',color:'#999999', fontFamily: 'IndustrialSans, sans-serif', marginTop: '5rem' }}>
         Funnel: <span style={{ color: 'white',letterSpacing: '0.5px'  }}>{selectedFunnel}</span>
       </h1>
-      <h2 style={{ fontSize: '15px', color:'#999999', fontFamily: 'inter, sans-serif',fontWeight:'300', marginTop: '1rem' }}>
+      <h2 style={{ fontSize: '15px', color:'#eeeeee', fontFamily: 'inter, sans-serif',fontWeight:'300', marginTop: '1rem' }}>
         Lead Pipeline: <span style={{ color: 'white',letterSpacing: '0.5px'  }}>{leadPipeline}</span>
       </h2>
-      <h2 style={{ fontSize: '15px',color:'#999999', fontFamily: 'inter, sans-serif',fontWeight:'300', marginTop: '0.5rem' }}>
+      <h2 style={{ fontSize: '15px',color:'#eeeeee', fontFamily: 'inter, sans-serif',fontWeight:'300', marginTop: '0.5rem' }}>
         BDR Pipeline: <span style={{ color: 'white',letterSpacing: '0.5px'  }}>{bdrPipeline}</span>
       </h2>
-      <h2 style={{ fontSize: '15px',color:'#999999', fontFamily: 'inter, sans-serif', fontWeight:'300', marginTop: '0.5rem', marginBottom:'0.5rem' }}>
+      <h2 style={{ fontSize: '15px',color:'#eeeeee', fontFamily: 'inter, sans-serif', fontWeight:'300', marginTop: '0.5rem', marginBottom:'0.5rem' }}>
         Sales Pipeline: <span style={{ color: 'white',letterSpacing: '0.5px'  }}>{salesPipeline}</span>
       </h2>
       </div>
