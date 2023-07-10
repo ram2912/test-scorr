a221e4f94be0ab177265a4d48ee93dadc62b099f
ram2912 <pawar.shriram10@gmail.com>
2023-07-10 11:37:06 +0200

    test

--- src/Components/ModelScreen.js
+++ src/Components/ModelScreen.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 import { Grid, Paper } from '@mui/material';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import Typography from '@mui/material/Typography';
